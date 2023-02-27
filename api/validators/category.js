4bdac1d0fc27d939b8efe258f247bbfe686bde06
Almarup21 <delltop231@gmail.com>
2023-02-27 12:20:07 +0700

    Success updating

--- api/validators/category.js
+++
@@ -1,9 +0,0 @@
-const { check } = require("express-validator");
-
-exports.categoryCreate = [
-  check("name")
-    .not()
-    .isEmpty()
-    .withMessage("Name is required")
-    .matches(/^[a-zA-Z\s'-]{1,100}$/),
-];
--- api/validators/index.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require("express-validator");
-
-exports.runValidationResult = (req, res, next) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res.status(422).json({ error: error.array()[0].msg });
-  }
-  next();
-};
